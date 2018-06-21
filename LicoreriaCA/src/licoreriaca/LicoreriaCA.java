99004cf6dade640f61f600173e5e5fcb493336e7
jmartinez <jersson.martinez@correounivalle.edu.co>
2018-06-20 20:55:11 -0500

    ejm

---
+++ LicoreriaCA/src/licoreriaca/LicoreriaCA.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package licoreriaca;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author Rodriguez
+ */
+public class LicoreriaCA {
+
+    private String nombredb = "licoreriaCA";
+    private String userdb = "postgres";
+    private String clavedb = "coom";
+    private int puerto = 5432;
+    private String host = "localhost";
+    private Connection con;
+    
+    
+    
+    public LicoreriaCA(){
+      //initComponents();
+     
+        
+    
+    
+}
+}
